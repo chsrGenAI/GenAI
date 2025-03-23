686e5270542b8d4ad634dbb1f8851a303cbd5fa9
Subba <chsr.genai@gmail.com>
2025-03-23 16:21:17 +0000

    google genai model

---
+++ chatmodel_google.py
@@ -0,0 +1,11 @@
+# pip install langchain-google-genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model='gemini-1.5-pro')
+
+result = model.invoke("What is the capital of India")
+
+print(result.content)
